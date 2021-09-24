4fd7cd8888b70d436cceeea45e5ad71176cf30ea
patternspandemic <patternspandemic@live.com>
2021-09-23 17:55:57 -0700

    More binding prep for kinc/graphics4.

--- source/scheme/kinc/graphics4/pipeline.scm
+++ source/scheme/kinc/graphics4/pipeline.scm
@@ -47,14 +47,53 @@
                                        KINC_G4_STENCIL_DECREMENT_WRAP
                                        KINC_G4_STENCIL_INVERT))
 
-;; void kinc_g4_pipeline_init (kinc_g4_pipeline_t *pipeline)
-;; void kinc_g4_pipeline_destroy (kinc_g4_pipeline_t *pipeline)
-;; void kinc_g4_pipeline_compile (kinc_g4_pipeline_t *pipeline)
-;; kinc_g4_constant_location_t kinc_g4_pipeline_get_constant_location (kinc_g4_pipeline_t *pipeline, const char *name)
-;; kinc_g4_texture_unit_t kinc_g4_pipeline_get_texture_unit (kinc_g4_pipeline_t *pipeline, const char *name)
-;; void kinc_g4_internal_set_pipeline (kinc_g4_pipeline_t *pipeline)
-;; void kinc_g4_internal_pipeline_set_defaults (kinc_g4_pipeline_t *pipeline)
+      ;; TODO: Functions requiring special C-object conversion
+      (in-C "
 
+static s7_pointer g_kinc_g4_pipeline_init(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_pipeline_init (kinc_g4_pipeline_t *pipeline)
+
+}
+
+static s7_pointer g_kinc_g4_pipeline_destroy(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_pipeline_destroy (kinc_g4_pipeline_t *pipeline)
+
+}
+
+static s7_pointer g_kinc_g4_pipeline_compile(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_pipeline_compile (kinc_g4_pipeline_t *pipeline)
+
+}
+
+static s7_pointer g_kinc_g4_pipeline_get_constant_location(s7_scheme *sc, s7_pointer args) {
+// kinc_g4_constant_location_t kinc_g4_pipeline_get_constant_location (kinc_g4_pipeline_t *pipeline, const char *name)
+
+}
+
+static s7_pointer g_kinc_g4_pipeline_get_texture_unit(s7_scheme *sc, s7_pointer args) {
+// kinc_g4_texture_unit_t kinc_g4_pipeline_get_texture_unit (kinc_g4_pipeline_t *pipeline, const char *name)
+
+}
+
+static s7_pointer g_kinc_g4_internal_set_pipeline(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_internal_set_pipeline (kinc_g4_pipeline_t *pipeline)
+
+}
+
+static s7_pointer g_kinc_g4_internal_pipeline_set_defaults(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_internal_pipeline_set_defaults (kinc_g4_pipeline_t *pipeline)
+
+}
+
+") ;; end special C-object conversion
+
+      (C-function ("kinc_g4_pipeline_init" g_kinc_g4_pipeline_init "void kinc_g4_pipeline_init (kinc_g4_pipeline_t *pipeline)" 1))
+      (C-function ("kinc_g4_pipeline_destroy" g_kinc_g4_pipeline_destroy "void kinc_g4_pipeline_destroy (kinc_g4_pipeline_t *pipeline)" 1))
+      (C-function ("kinc_g4_pipeline_compile" g_kinc_g4_pipeline_compile "void kinc_g4_pipeline_compile (kinc_g4_pipeline_t *pipeline)" 1))
+      (C-function ("kinc_g4_pipeline_get_constant_location" g_kinc_g4_pipeline_get_constant_location "kinc_g4_constant_location_t kinc_g4_pipeline_get_constant_location (kinc_g4_pipeline_t *pipeline, const char *name)" 2))
+      (C-function ("kinc_g4_pipeline_get_texture_unit" g_kinc_g4_pipeline_get_texture_unit "kinc_g4_texture_unit_t kinc_g4_pipeline_get_texture_unit (kinc_g4_pipeline_t *pipeline, const char *name)" 2))
+      (C-function ("kinc_g4_internal_set_pipeline" g_kinc_g4_internal_set_pipeline "void kinc_g4_internal_set_pipeline (kinc_g4_pipeline_t *pipeline)" 1))
+      (C-function ("kinc_g4_internal_pipeline_set_defaults" g_kinc_g4_internal_pipeline_set_defaults "void kinc_g4_internal_pipeline_set_defaults (kinc_g4_pipeline_t *pipeline)" 1))
     )
   )
 
--- source/scheme/kinc/graphics4/rendertarget.scm
+++ source/scheme/kinc/graphics4/rendertarget.scm
@@ -22,15 +22,59 @@
                                              KINC_G4_RENDER_TARGET_FORMAT_8BIT_RED
                                              KINC_G4_RENDER_TARGET_FORMAT_16BIT_RED_FLOAT))
 
-;; void kinc_g4_render_target_init (kinc_g4_render_target_t *renderTarget, int width, int height, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)
-;; void kinc_g4_render_target_init_cube (kinc_g4_render_target_t *renderTarget, int cubeMapSize, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)
-;; void kinc_g4_render_target_destroy (kinc_g4_render_target_t *renderTarget)
-;; void kinc_g4_render_target_use_color_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)
-;; void kinc_g4_render_target_use_depth_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)
-;; void kinc_g4_render_target_set_depth_stencil_from (kinc_g4_render_target_t *renderTarget, kinc_g4_render_target_t *source)
-;; void kinc_g4_render_target_get_pixels (kinc_g4_render_target_t *renderTarget, uint8_t *data)
-;; void kinc_g4_render_target_generate_mipmaps (kinc_g4_render_target_t *renderTarget, int levels)
+      ;; TODO: Functions requiring special C-object conversion
+      (in-C "
 
+static s7_pointer g_kinc_g4_render_target_init(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_init (kinc_g4_render_target_t *renderTarget, int width, int height, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_init_cube(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_init_cube (kinc_g4_render_target_t *renderTarget, int cubeMapSize, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_destroy(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_destroy (kinc_g4_render_target_t *renderTarget)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_use_color_as_texture(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_use_color_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_use_depth_as_texture(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_use_depth_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_set_depth_stencil_from(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_set_depth_stencil_from (kinc_g4_render_target_t *renderTarget, kinc_g4_render_target_t *source)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_get_pixels(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_get_pixels (kinc_g4_render_target_t *renderTarget, uint8_t *data)
+
+}
+
+static s7_pointer g_kinc_g4_render_target_generate_mipmaps(s7_scheme *sc, s7_pointer args) {
+// void kinc_g4_render_target_generate_mipmaps (kinc_g4_render_target_t *renderTarget, int levels)
+
+}
+
+") ;; end special C-object conversion
+
+      (C-function ("kinc_g4_render_target_init" g_kinc_g4_render_target_init "void kinc_g4_render_target_init (kinc_g4_render_target_t *renderTarget, int width, int height, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)" 8))
+      (C-function ("kinc_g4_render_target_init_cube" g_kinc_g4_render_target_init_cube "void kinc_g4_render_target_init_cube (kinc_g4_render_target_t *renderTarget, int cubeMapSize, int depthBufferBits, bool antialiasing, kinc_g4_render_target_format_t format, int stencilBufferBits, int contextId)" 7))
+      (C-function ("kinc_g4_render_target_destroy" g_kinc_g4_render_target_destroy "void kinc_g4_render_target_destroy (kinc_g4_render_target_t *renderTarget)" 1))
+      (C-function ("kinc_g4_render_target_use_color_as_texture" g_kinc_g4_render_target_use_color_as_texture "void kinc_g4_render_target_use_color_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)" 2))
+      (C-function ("kinc_g4_render_target_use_depth_as_texture" g_kinc_g4_render_target_use_depth_as_texture "void kinc_g4_render_target_use_depth_as_texture (kinc_g4_render_target_t *renderTarget, kinc_g4_texture_unit_t unit)" 2))
+      (C-function ("kinc_g4_render_target_set_depth_stencil_from" g_kinc_g4_render_target_set_depth_stencil_from "void kinc_g4_render_target_set_depth_stencil_from (kinc_g4_render_target_t *renderTarget, kinc_g4_render_target_t *source)" 2))
+      (C-function ("kinc_g4_render_target_get_pixels" g_kinc_g4_render_target_get_pixels "void kinc_g4_render_target_get_pixels (kinc_g4_render_target_t *renderTarget, uint8_t *data)" 2))
+      (C-function ("kinc_g4_render_target_generate_mipmaps" g_kinc_g4_render_target_generate_mipmaps "void kinc_g4_render_target_generate_mipmaps (kinc_g4_render_target_t *renderTarget, int levels)" 2))
     )
   )
 
